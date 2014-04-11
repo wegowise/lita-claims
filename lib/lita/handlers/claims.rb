74566232828262e27b2d0670f31d0d00bdc1c946
Hannes Fostie <hannes@openminds.be>
2014-04-11 16:06:17 +0200

    Adds basic create routes/commands

--- lib/lita/handlers/claims.rb
+++ lib/lita/handlers/claims.rb
@@ -1,8 +1,28 @@
 require "lita"
+require 'pry'
 
 module Lita
   module Handlers
     class Claims < Handler
+      PROPERTY_OR_ENVIRONMENT = /[\w\-]+/
+
+      route(/^claim\s(#{PROPERTY_OR_ENVIRONMENT.source})(?:\s(#{PROPERTY_OR_ENVIRONMENT.source}))?/i, :create, command: true, help: {
+        "claim PROPERTY" => "To claim a property by the name PROPERTY"
+      })
+
+      def create(response)
+        claimer = response.message.source.user.name
+        property, environment = response.matches.first
+        environment ||= 'default'
+        environment_string = " (#{environment})" if environment != 'default'
+        if property && Claim.create(property, claimer, environment)
+          reply = "#{property}#{environment_string} was claimed by #{claimer}"
+        else
+          existing_claimer = Claim.read(property, environment)
+          reply = "Could not claim #{property}#{environment_string} - already claimed by #{existing_claimer}"
+        end
+        response.reply(reply)
+      end
     end
 
     Lita.register_handler(Claims)
--- spec/lita/handlers/claims_spec.rb
+++ spec/lita/handlers/claims_spec.rb
@@ -1,4 +1,32 @@
 require "spec_helper"
 
 describe Lita::Handlers::Claims, lita_handler: true do
+  describe 'routes' do
+    it { routes_command('claim property').to :create }
+    it { routes_command('claim property environment').to :create }
+  end
+
+  describe 'creating claims' do
+    it 'replies that the claim was made' do
+      send_command 'claim property_x'
+      expect(replies.last).to eq('property_x was claimed by Test User')
+    end
+
+    it 'replies that the claim was made for a non-default environment' do
+      send_command 'claim property_x environment_y'
+      expect(replies.last).to eq('property_x (environment_y) was claimed by Test User')
+    end
+
+    it 'replies that a claim could not be made because it already exists' do
+      Claim.create('property', 'Test User')
+      send_command 'claim property'
+      expect(replies.last).to eq('Could not claim property - already claimed by Test User')
+    end
+
+    it 'replies that a claim could not be made for a non-default environment because it already exists' do
+      Claim.create('property', 'Test User', 'env')
+      send_command 'claim property env'
+      expect(replies.last).to eq('Could not claim property (env) - already claimed by Test User')
+    end
+  end
 end
