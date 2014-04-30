1e4e5fc8da39f00a71d79e4aef4262f076edf930
Hannes Fostie <hannes@openminds.be>
2014-04-30 17:04:36 +0200

    Adds 'who claimed' command, improves tests

--- lib/lita/handlers/claims.rb
+++ lib/lita/handlers/claims.rb
@@ -10,16 +10,33 @@ module Lita
         "claim PROPERTY" => "To claim a property by the name PROPERTY"
       })
 
+      route(/^who claimed\s(#{PROPERTY_OR_ENVIRONMENT.source})(?:\s(#{PROPERTY_OR_ENVIRONMENT.source}))?/i, :exists?, command: true, help: {
+        "who claimed PROPERTY" => "To see who claimed a property by the name PROPERTY"
+      })
+
       def create(response)
         claimer = response.message.source.user.name
         property, environment = response.matches.first
         environment ||= 'default'
         environment_string = " (#{environment})" if environment != 'default'
         if property && Claim.create(property, claimer, environment)
-          reply = "#{property}#{environment_string} was claimed by #{claimer}"
+          reply = "#{property}#{environment_string} was claimed by #{claimer}."
         else
           existing_claimer = Claim.read(property, environment)
-          reply = "Could not claim #{property}#{environment_string} - already claimed by #{existing_claimer}"
+          reply = "Could not claim #{property}#{environment_string} - already claimed by #{existing_claimer}."
+        end
+        response.reply(reply)
+      end
+
+      def exists?(response)
+        property, environment = response.matches.first
+        environment ||= 'default'
+        environment_string = " (#{environment})" if environment != 'default'
+        if property && Claim.exists?(property, environment)
+          claimer = Claim.read(property, environment)
+          reply = "#{property}#{environment_string} is currently claimed by #{claimer}."
+        else
+          reply = "#{property}#{environment_string} has not been claimed."
         end
         response.reply(reply)
       end
--- spec/lita/handlers/claims_spec.rb
+++ spec/lita/handlers/claims_spec.rb
@@ -4,29 +4,58 @@ describe Lita::Handlers::Claims, lita_handler: true do
   describe 'routes' do
     it { routes_command('claim property').to :create }
     it { routes_command('claim property environment').to :create }
+
+    it { routes_command('who claimed property').to :exists? }
+    it { routes_command('who claimed property environment').to :exists? }
   end
 
   describe 'creating claims' do
     it 'replies that the claim was made' do
       send_command 'claim property_x'
-      expect(replies.last).to eq('property_x was claimed by Test User')
+      expect(replies.last).to eq('property_x was claimed by Test User.')
+      expect(Claim.exists?('property_x')).to be_true
     end
 
     it 'replies that the claim was made for a non-default environment' do
       send_command 'claim property_x environment_y'
-      expect(replies.last).to eq('property_x (environment_y) was claimed by Test User')
+      expect(replies.last).to eq('property_x (environment_y) was claimed by Test User.')
+      expect(Claim.exists?('property_x', 'environment_y')).to be_true
     end
 
     it 'replies that a claim could not be made because it already exists' do
       Claim.create('property', 'Test User')
       send_command 'claim property'
-      expect(replies.last).to eq('Could not claim property - already claimed by Test User')
+      expect(replies.last).to eq('Could not claim property - already claimed by Test User.')
     end
 
     it 'replies that a claim could not be made for a non-default environment because it already exists' do
       Claim.create('property', 'Test User', 'env')
       send_command 'claim property env'
-      expect(replies.last).to eq('Could not claim property (env) - already claimed by Test User')
+      expect(replies.last).to eq('Could not claim property (env) - already claimed by Test User.')
+    end
+  end
+
+  describe 'check if property has been claimed' do
+    it 'replies who claimed a property' do
+      Claim.create('property_x', 'claimer')
+      send_command 'who claimed property_x'
+      expect(replies.last).to eq('property_x is currently claimed by claimer.')
+    end
+
+    it 'replies who claimed a property with environment' do
+      Claim.create('property_x', 'claimer', 'some_env')
+      send_command 'who claimed property_x some_env'
+      expect(replies.last).to eq('property_x (some_env) is currently claimed by claimer.')
+    end
+
+    it 'replies that a property has not been claimed' do
+      send_command 'who claimed property_x'
+      expect(replies.last).to eq('property_x has not been claimed.')
+    end
+
+    it 'replies that a property has not been claimed with environment' do
+      send_command 'who claimed property_x some_env'
+      expect(replies.last).to eq('property_x (some_env) has not been claimed.')
     end
   end
 end
